e7719c293d9c095c87485525a09ab2fb2defdd84
dfvhncdmfjvncx <48837446+dfvhncdmfjvncx@users.noreply.github.com>
2019-10-19 16:35:30 -0400

    Add files via upload

---
+++ Chapters 3-6 puzzles.py
@@ -0,0 +1,70 @@
+import turtle
+
+#3-1
+hobbies = ['coding','building']
+favorite_foods = ['tacos','rice']
+favorites = favorite_foods + hobbies
+print(favorites)
+
+#3-2
+ninjas = 3 * 20 + 2 * 40
+
+#3-3
+firstname='Thanish'
+lastname='Boggula'
+print('hello ' + firstname + ' ' + lastname)
+
+#4-1
+t=turtle.pen()
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+
+#4-2
+t.forward(50)
+t.left(60)
+t.forward(50)
+t.left(60)
+t.forward(50)
+
+#4-3
+t.forward(80)
+t.penup()
+t.forward(20)
+t.left(90)
+t.forward(20)
+t.pendown()
+t.forward(80)
+t.penup()
+t.forward(20)
+t.left(90)
+t.forward(20)
+t.pendown()
+t.forward(80)
+t.penup()
+t.forward(20)
+t.left(90)
+t.forward(20)
+t.pendown()
+t.forward(80)
+t.penup()
+t.forward(20)
+t.left(90)
+t.forward(20)
+t.pendown()
+
+#5-1
+# I'm rich!!
+
+#5-2
+twinkies = 1023102384
+if twinkies > 100:
+    print('You are going to die')
+else:
+    print('healthy amount')
+
+#5-3
+money = -1000
